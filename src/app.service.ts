e5bf024677b9c0de764932e1baa03aaa74cc706e
YoonWonYoul <yoonwonyoul@webchemist.net>
2023-12-30 11:13:24 +0900

    chore: calc date column

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ import { ConfigService } from '@nestjs/config';
 export class AppService {
   constructor(private config: ConfigService) {}
 
-  async getHello(): Promise<any> {
+  async getCalendar(): Promise<any> {
     const notionSecretKey = this.config.get('NOTION_SECRET_KEY');
     const notionDatabaseId = this.config.get('NOTION_DATABASE_ID');
 
@@ -21,18 +21,34 @@ export class AppService {
 
       const events = response.results.map<any>(({ properties }: any) => {
         const { 이름, 날짜 } = properties;
-
         const { start, end } = 날짜['date'];
+
+        let startData = [
+          Number(dayjs(start).format('YYYY')),
+          Number(dayjs(start).format('MM')),
+          Number(dayjs(start).format('DD')),
+        ];
+
+        if (end) {
+          startData = [
+            ...startData,
+            Number(dayjs(start).format('HH')),
+            Number(dayjs(start).format('mm')),
+          ];
+        }
+
         const result = {
           title: 이름['title'][0].text.content,
-          start: [
-            Number(dayjs(start).format('YYYY')),
-            Number(dayjs(start).format('MM')),
-            Number(dayjs(start).format('DD')),
-          ],
+          start: startData,
         };
 
         if (end) {
+          const date = Number(dayjs(end).diff(dayjs(start), 'minutes'));
+          const minutes = date % 60;
+          let hour = date / 60;
+          const days = hour / 24 > 0 ? hour / 24 : 0;
+          hour = days % 24;
+          console.log(days, hour, minutes);
           result['duration'] = {
             days: Number(dayjs(end).diff(dayjs(start), 'days')) + 1,
           };
