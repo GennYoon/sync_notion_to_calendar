1ea65509b5e7738b8fb8fb3befce31ada71e72fa
YoonWonYoul <yoonwonyoul@webchemist.net>
2024-01-01 11:47:09 +0900

    chore: change Lambda locale to ko

--- .build/src/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class AppService {
-    private config;
-    constructor(config: ConfigService);
-    getCalendar(): Promise<any>;
-}
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,22 +1,22 @@
-import { Handler, Context } from 'aws-lambda';
+import express from 'express';
+import 'dayjs/locale/ko';
+import dayjs from 'dayjs';
 import { Server } from 'http';
+import { Handler, Context } from 'aws-lambda';
 import { createServer, proxy } from 'aws-serverless-express';
 import { eventContext } from 'aws-serverless-express/middleware';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import express from 'express';
 const binaryMimeTypes: string[] = [];
 
 let cachedServer: Server;
 
 async function bootstrapServer(): Promise<Server> {
+  dayjs.locale('ko');
   if (!cachedServer) {
     const expressApp = express();
-    const nestApp = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-    );
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
     nestApp.use(eventContext());
     await nestApp.init();
     cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
